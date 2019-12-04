42b46bfb92fc29b2ca61cd28f21dbcdabbb67874
yuhuahe06 <47650674+yuhuahe06@users.noreply.github.com>
2019-12-03 22:31:23 -0800

    visualizeLayers method created

--- HandEmojiCNN.py
+++ HandEmojiCNN.py
@@ -242,92 +242,85 @@ def guessGesture(model, img):
     else:
         # Lets return index 1 for 'Nothing' 
         return 1
+def visualizeHis(hist):
+    # visualizing losses and accuracy
 
-def initializers():
-    imlist = modlistdir(path2)
-    
-    image1 = np.array(Image.open(path2 +'/' + imlist[0])) # open one image to get size
-    #plt.imshow(im1)
-    
-    m,n = image1.shape[0:2] # get the size of the images
-    total_images = len(imlist) # get the 'total' number of images
-    
-    # create matrix to store all flattened images
-    immatrix = np.array([np.array(Image.open(path2+ '/' + images).convert('L')).flatten()
-                         for images in sorted(imlist)], dtype = 'f')
-    
+    train_loss=hist.history['loss']
+    val_loss=hist.history['val_loss']
+    train_acc=hist.history['acc']
+    val_acc=hist.history['val_acc']
+    xc=range(nb_epoch)
 
-    
-    print(immatrix.shape)
-    
-    input("Press any key")
-    
-    #########################################################
-    ## Label the set of images per respective gesture type.
-    ##
-    label=np.ones((total_images,),dtype = int)
-    
-    samples_per_class = int(total_images / nb_classes)
-    print("samples_per_class - ",samples_per_class)
-    s = 0
-    r = samples_per_class
-    for classIndex in range(nb_classes):
-        label[s:r] = classIndex
-        s = r
-        r = s + samples_per_class
-    
-    '''
-    # eg: For 301 img samples/gesture for 4 gesture types
-    label[0:301]=0
-    label[301:602]=1
-    label[602:903]=2
-    label[903:]=3
-    '''
-    
-    data,Label = shuffle(immatrix,label, random_state=2)
-    train_data = [data,Label]
-     
-    (X, y) = (train_data[0],train_data[1])
-     
-     
-    # Split X and y into training and testing sets
-     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
-     
-    X_train = X_train.reshape(X_train.shape[0], img_channels, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], img_channels, img_rows, img_cols)
-     
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-     
-    # normalize
-    X_train /= 255
-    X_test /= 255
-     
-    # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
-    return X_train, X_test, Y_train, Y_test
+    plt.figure(1,figsize=(7,5))
+    plt.plot(xc,train_loss)
+    plt.plot(xc,val_loss)
+    plt.xlabel('num of Epochs')
+    plt.ylabel('loss')
+    plt.title('train_loss vs val_loss')
+    plt.grid(True)
+    plt.legend(['train','val'])
+    #print plt.style.available # use bmh, classic,ggplot for big pictures
+    #plt.style.use(['classic'])
 
+    plt.figure(2,figsize=(7,5))
+    plt.plot(xc,train_acc)
+    plt.plot(xc,val_acc)
+    plt.xlabel('num of Epochs')
+    plt.ylabel('accuracy')
+    plt.title('train_acc vs val_acc')
+    plt.grid(True)
+    plt.legend(['train','val'],loc=4)
 
+    plt.show()
 
-def trainModel(model):
-
-    # Split X and y into training and testing sets
-    X_train, X_test, Y_train, Y_test = initializers()
-
-    # Now start the training of the loaded model
-    hist = model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
-                 verbose=1, validation_split=0.2)
-
-    visualizeHis(hist)
-
-    ans = input("Do you want to save the trained weights - y/n ?")
-    if ans == 'y':
-        filename = input("Enter file name - ")
-        fname = path + str(filename) + ".hdf5"
-        model.save_weights(fname,overwrite=True)
+#%%
+def visualizeLayers(model):
+    imlist = modlistdir('./imgs')
+    if len(imlist) == 0:
+        print('Error: No sample image file found under \'./imgs\' folder.')
+        return
     else:
-        model.save_weights("newWeight.hdf5",overwrite=True)
+        print('Found these sample image files - {}'.format(imlist))
 
-    # Save model as well
+    img = int(input("Which sample image file to load (enter the INDEX of it, which starts from 0): "))
+    layerIndex = int(input("Enter which layer to visualize. Enter -1 to visualize all layers possible: "))
+    
+    if img <= len(imlist):
+        
+        image = np.array(Image.open('./imgs/' + imlist[img]).convert('L')).flatten()
+        
+        ## Predict
+        print('Guessed Gesture is {}'.format(output[guessGesture(model,image)]))
+        
+        # reshape it
+        image = image.reshape(img_channels, img_rows,img_cols)
+        
+        # float32
+        image = image.astype('float32')
+        
+        # normalize it
+        image = image / 255
+        
+        # reshape for NN
+        input_image = image.reshape(1, img_channels, img_rows, img_cols)
+    else:
+        print('Wrong file index entered !!')
+        return
+    
+    
+    
+        
+    # visualizing intermediate layers
+    #output_layer = model.layers[layerIndex].output
+    #output_fn = theano.function([model.layers[0].input], output_layer)
+    #output_image = output_fn(input_image)
+    
+    if layerIndex >= 1:
+        visualizeLayer(model,img,input_image, layerIndex)
+    else:
+        tlayers = len(model.layers[:])
+        print("Total layers - {}".format(tlayers))
+        for i in range(1,tlayers):
+             visualizeLayer(model,img, input_image,i)
+
+#%%
