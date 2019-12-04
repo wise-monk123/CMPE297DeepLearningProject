e786ba9ad4b938ec52bff3f1d61bfbb68b02c92a
Guitarja <124254388@qq.com>
2019-12-03 22:30:46 -0800

    train part

--- HandEmojiCNN.py
+++ HandEmojiCNN.py
@@ -242,3 +242,92 @@ def guessGesture(model, img):
     else:
         # Lets return index 1 for 'Nothing' 
         return 1
+
+def initializers():
+    imlist = modlistdir(path2)
+    
+    image1 = np.array(Image.open(path2 +'/' + imlist[0])) # open one image to get size
+    #plt.imshow(im1)
+    
+    m,n = image1.shape[0:2] # get the size of the images
+    total_images = len(imlist) # get the 'total' number of images
+    
+    # create matrix to store all flattened images
+    immatrix = np.array([np.array(Image.open(path2+ '/' + images).convert('L')).flatten()
+                         for images in sorted(imlist)], dtype = 'f')
+    
+
+    
+    print(immatrix.shape)
+    
+    input("Press any key")
+    
+    #########################################################
+    ## Label the set of images per respective gesture type.
+    ##
+    label=np.ones((total_images,),dtype = int)
+    
+    samples_per_class = int(total_images / nb_classes)
+    print("samples_per_class - ",samples_per_class)
+    s = 0
+    r = samples_per_class
+    for classIndex in range(nb_classes):
+        label[s:r] = classIndex
+        s = r
+        r = s + samples_per_class
+    
+    '''
+    # eg: For 301 img samples/gesture for 4 gesture types
+    label[0:301]=0
+    label[301:602]=1
+    label[602:903]=2
+    label[903:]=3
+    '''
+    
+    data,Label = shuffle(immatrix,label, random_state=2)
+    train_data = [data,Label]
+     
+    (X, y) = (train_data[0],train_data[1])
+     
+     
+    # Split X and y into training and testing sets
+     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+     
+    X_train = X_train.reshape(X_train.shape[0], img_channels, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], img_channels, img_rows, img_cols)
+     
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+     
+    # normalize
+    X_train /= 255
+    X_test /= 255
+     
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, X_test, Y_train, Y_test
+
+
+
+def trainModel(model):
+
+    # Split X and y into training and testing sets
+    X_train, X_test, Y_train, Y_test = initializers()
+
+    # Now start the training of the loaded model
+    hist = model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
+                 verbose=1, validation_split=0.2)
+
+    visualizeHis(hist)
+
+    ans = input("Do you want to save the trained weights - y/n ?")
+    if ans == 'y':
+        filename = input("Enter file name - ")
+        fname = path + str(filename) + ".hdf5"
+        model.save_weights(fname,overwrite=True)
+    else:
+        model.save_weights("newWeight.hdf5",overwrite=True)
+
+    # Save model as well
